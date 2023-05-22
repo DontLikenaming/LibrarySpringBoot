f56bde4a4dcff652388789bc991ae6f6364f1f98
DontLikenaming <edirna21@gmail.com>
2023-05-22 17:56:57 +0900

    Library Repository 인터페이스 구현

---
+++ src/main/java/exam/springboot/jpa/library/repository/LibraryRepository.java
@@ -0,0 +1,9 @@
+package exam.springboot.jpa.library.repository;
+
+import exam.springboot.jpa.library.model.Library;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface LibraryRepository extends PagingAndSortingRepository<Library, Long> {
+
+    int countLibraryBy();
+}
