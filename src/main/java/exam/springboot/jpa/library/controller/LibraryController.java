a8ed93abf319e23fd2b2c0eb295a4a8940184a6f
DontLikenaming <edirna21@gmail.com>
2023-05-22 17:54:54 +0900

    controller에서 library 서비스 호출

--- src/main/java/exam/springboot/jpa/library/controller/LibraryController.java
+++ src/main/java/exam/springboot/jpa/library/controller/LibraryController.java
@@ -2,34 +2,23 @@ package exam.springboot.jpa.library.controller;
 
 import exam.springboot.jpa.library.service.LibraryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 @RequestMapping(value = "/")
 public class LibraryController {
     @Autowired private LibraryService libsrv;
 
     @GetMapping(value = "/")
-    public ModelAndView path(Integer page){
-        int cntpg = libsrv.countBoard();
-        if((page==null)||(page<=0)){page = 1;}
-        else if((page>cntpg)){page = cntpg;}
-
-        ModelAndView mv = new ModelAndView();
-        mv.setViewName("/list");
-        mv.addObject("lblist", libsrv.readBoard(page));
-        mv.addObject("page", page);
-        mv.addObject("stpg", (page-1)/25*25+1);
-        mv.addObject("cntpg", cntpg);
-
-        return mv;
+    public String list(){
+        return "forward:/list";
     }
 
     @GetMapping(value = "/list")
-    public ModelAndView list(Integer page){
+    public ModelAndView list2(Integer page){
         int cntpg = libsrv.countBoard();
         if((page==null)||(page<=0)){page = 1;}
         else if((page>cntpg)){page = cntpg;}
